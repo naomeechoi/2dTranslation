c33d2888fc74bde509bb5e26d55582bfeb7109ce
naomi choi <fhdjskal2@naver.com>
2023-11-17 10:35:36 +0900

    Camera Matrix

--- app.js
+++ app.js
@@ -44,12 +44,22 @@ window.onload = function () {
   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
   setColors(gl);
 
-  var translation = [1, 1, 1];
-  var rotation = [1, 1, 1];
-  var scale = [1, 1, 1];
-  var fieldOfViewRadians = degreesToRadians(1);
+  var cameraAngleRadians = degreesToRadians(0);
+  var fieldOfViewRadians = degreesToRadians(60);
 
-  // 위치 슬라이드바 설정
+  // 카메라 앵글 슬라이드바 설정
+  function updateCameraAngle(ele) {
+    cameraAngleRadians = degreesToRadians(ele.target.value);
+    ele.target.previousSibling.innerHTML =
+      ele.target.previousSibling.getAttribute("name") + ele.target.value;
+    drawScene();
+  }
+
+  makeSliderAndRedraw("cameraAngleAngle: ", "perspectiveSlider", 0, 0, 360, (ele) => {
+    return updateCameraAngle(ele);
+  });
+
+  // 원근 각도 슬라이드바 설정
   function updatePerspective(ele) {
     fieldOfViewRadians = degreesToRadians(ele.target.value);
     ele.target.previousSibling.innerHTML =
@@ -57,75 +67,12 @@ window.onload = function () {
     drawScene();
   }
 
-  makeSliderAndRedraw("perspectiveAngle: ", "perspectiveSlider", 1, 360, (ele) => {
+  makeSliderAndRedraw("perspectiveAngle: ", "perspectiveSlider", 60, 1, 360, (ele) => {
     return updatePerspective(ele);
   });
 
-  // 위치 슬라이드바 설정
-  function updatePosition(ele, index) {
-    translation[index] = ele.target.value;
-    ele.target.previousSibling.innerHTML =
-      ele.target.previousSibling.getAttribute("name") + ele.target.value;
-    drawScene();
-  }
 
-  makeSliderAndRedraw("translation X: ", "translationSlider", -200, 200, (ele) => {
-    return updatePosition(ele, 0);
-  });
-
-  makeSliderAndRedraw("translation Y: ", "translationSlider", -200, 200, (ele) => {
-    return updatePosition(ele, 1);
-  });
-
-  makeSliderAndRedraw("translation Z: ", "translationSlider", -1000, 1, (ele) => {
-    return updatePosition(ele, 2);
-  });
-
-  // 회전 슬라이드 x 바 설정
-  function updateAngle(ele, index) {
-    rotation[index] = degreesToRadians(360 - ele.target.value);
-
-    ele.target.previousSibling.innerHTML =
-      ele.target.previousSibling.getAttribute("name") + ele.target.value;
-    drawScene();
-  }
-
-  makeSliderAndRedraw("angle X: ", "rotationSlider", 0, 360, (ele) => {
-    return updateAngle(ele, 0);
-  });
-
-  makeSliderAndRedraw("angle Y: ", "rotationSlider", 0, 360, (ele) => {
-    return updateAngle(ele, 1);
-  });
-
-
-  makeSliderAndRedraw("angle Z: ", "rotationSlider", 0, 360, (ele) => {
-    return updateAngle(ele, 2);
-  });
-
-
-  // 스케일 슬라이드 x 바 설정
-  function updateScale(ele, index) {
-    scale[index] = ele.target.value;
-
-    ele.target.previousSibling.innerHTML =
-      ele.target.previousSibling.getAttribute("name") + ele.target.value;
-    drawScene();
-  }
-
-  makeSliderAndRedraw("scale X: ", "scaleSlider", 0, 10, (ele) => {
-    return updateScale(ele, 0);
-  });
-
-  makeSliderAndRedraw("scale Y: ", "scaleSlider", 0, 10, (ele) => {
-    return updateScale(ele, 1);
-  });
-
-  makeSliderAndRedraw("scale Z: ", "scaleSlider", 0, 10, (ele) => {
-    return updateScale(ele, 2);
-  });
-
-  function makeSliderAndRedraw(eleName, parentDivId, min, max, func) {
+  function makeSliderAndRedraw(eleName, parentDivId, baseValue, min, max, func) {
     var parentDiv = document.getElementById(parentDivId);
 
     var division = document.createElement("d");
@@ -136,7 +83,7 @@ window.onload = function () {
     tempSlider.min = min;
     tempSlider.max = max;
     tempSlider.step = 0.01;
-    tempSlider.value = 1;
+    tempSlider.value = baseValue;
 
     var text = document.createElement("p");
     text.setAttribute("name", eleName);
@@ -190,25 +137,38 @@ window.onload = function () {
     gl.vertexAttribPointer(
       colorLocation, colorSize, colorType, colorNormalize, colorStride, colorOffset);
 
+
+    var numSs = 5;
+    var radius = 200;
     // Compute the matrices
     var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
     var zNear = 1;
     var zFar = 2000;
-    var matrix = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
-    console.log(matrix);
-    matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
-    matrix = m4.xRotate(matrix, rotation[0]);
-    matrix = m4.yRotate(matrix, rotation[1]);
-    matrix = m4.zRotate(matrix, rotation[2]);
-    matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
+    var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
 
-    gl.uniformMatrix4fv(matrixLocation, false, matrix);
+    var cameraMatrix = m4.yRotation(cameraAngleRadians);
+    cameraMatrix = m4.translate(cameraMatrix, 0, 0, radius * 1.5);
 
-    // Draw the rectangle.
-    var primitiveType = gl.TRIANGLES;
-    var offset = 0;
-    var count = 15 * 2 * 6;
-    gl.drawArrays(primitiveType, offset, count);
+    var viewMatrix = m4.inverse(cameraMatrix);
+    var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
+
+    for(var i = 0; i < numSs; i++)
+    {
+      var angle = i * Math.PI * 2 / numSs;
+      var x = Math.cos(angle) * radius;
+      var y = Math.sin(angle) * radius;
+
+      var matrix = m4.translate(viewProjectionMatrix, x, 0, y);
+
+  
+      gl.uniformMatrix4fv(matrixLocation, false, matrix);
+  
+      // Draw the rectangle.
+      var primitiveType = gl.TRIANGLES;
+      var offset = 0;
+      var count = 15 * 2 * 6;
+      gl.drawArrays(primitiveType, offset, count);
+    }
   }
 };
 
@@ -747,6 +707,91 @@ var m4 = {
 
   scale: function(m, sx, sy, sz) {
     return m4.multiply(m, m4.scaling(sx, sy, sz));
+  },
+
+  inverse: function(m) {
+    var m00 = m[0 * 4 + 0];
+    var m01 = m[0 * 4 + 1];
+    var m02 = m[0 * 4 + 2];
+    var m03 = m[0 * 4 + 3];
+    var m10 = m[1 * 4 + 0];
+    var m11 = m[1 * 4 + 1];
+    var m12 = m[1 * 4 + 2];
+    var m13 = m[1 * 4 + 3];
+    var m20 = m[2 * 4 + 0];
+    var m21 = m[2 * 4 + 1];
+    var m22 = m[2 * 4 + 2];
+    var m23 = m[2 * 4 + 3];
+    var m30 = m[3 * 4 + 0];
+    var m31 = m[3 * 4 + 1];
+    var m32 = m[3 * 4 + 2];
+    var m33 = m[3 * 4 + 3];
+    var tmp_0  = m22 * m33;
+    var tmp_1  = m32 * m23;
+    var tmp_2  = m12 * m33;
+    var tmp_3  = m32 * m13;
+    var tmp_4  = m12 * m23;
+    var tmp_5  = m22 * m13;
+    var tmp_6  = m02 * m33;
+    var tmp_7  = m32 * m03;
+    var tmp_8  = m02 * m23;
+    var tmp_9  = m22 * m03;
+    var tmp_10 = m02 * m13;
+    var tmp_11 = m12 * m03;
+    var tmp_12 = m20 * m31;
+    var tmp_13 = m30 * m21;
+    var tmp_14 = m10 * m31;
+    var tmp_15 = m30 * m11;
+    var tmp_16 = m10 * m21;
+    var tmp_17 = m20 * m11;
+    var tmp_18 = m00 * m31;
+    var tmp_19 = m30 * m01;
+    var tmp_20 = m00 * m21;
+    var tmp_21 = m20 * m01;
+    var tmp_22 = m00 * m11;
+    var tmp_23 = m10 * m01;
+
+    var t0 = (tmp_0 * m11 + tmp_3 * m21 + tmp_4 * m31) -
+        (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
+    var t1 = (tmp_1 * m01 + tmp_6 * m21 + tmp_9 * m31) -
+        (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
+    var t2 = (tmp_2 * m01 + tmp_7 * m11 + tmp_10 * m31) -
+        (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
+    var t3 = (tmp_5 * m01 + tmp_8 * m11 + tmp_11 * m21) -
+        (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+
+    var d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
+
+    return [
+      d * t0,
+      d * t1,
+      d * t2,
+      d * t3,
+      d * ((tmp_1 * m10 + tmp_2 * m20 + tmp_5 * m30) -
+            (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
+      d * ((tmp_0 * m00 + tmp_7 * m20 + tmp_8 * m30) -
+            (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
+      d * ((tmp_3 * m00 + tmp_6 * m10 + tmp_11 * m30) -
+            (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
+      d * ((tmp_4 * m00 + tmp_9 * m10 + tmp_10 * m20) -
+            (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
+      d * ((tmp_12 * m13 + tmp_15 * m23 + tmp_16 * m33) -
+            (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
+      d * ((tmp_13 * m03 + tmp_18 * m23 + tmp_21 * m33) -
+            (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
+      d * ((tmp_14 * m03 + tmp_19 * m13 + tmp_22 * m33) -
+            (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
+      d * ((tmp_17 * m03 + tmp_20 * m13 + tmp_23 * m23) -
+            (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
+      d * ((tmp_14 * m22 + tmp_17 * m32 + tmp_13 * m12) -
+            (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
+      d * ((tmp_20 * m32 + tmp_12 * m02 + tmp_19 * m22) -
+            (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
+      d * ((tmp_18 * m12 + tmp_23 * m32 + tmp_15 * m02) -
+            (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
+      d * ((tmp_22 * m22 + tmp_16 * m02 + tmp_21 * m12) -
+            (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02))
+    ];
   },
 
 };
