156f2f09ef32c354e73d7a2e5bb3834c45c7cff7
naomi choi <fhdjskal2@naver.com>
2023-11-17 10:36:14 +0900

    Camera Target Matrix

--- app.js
+++ app.js
@@ -146,8 +146,18 @@ window.onload = function () {
     var zFar = 2000;
     var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
 
+    var fPosition = [radius, 0, 0];
+
     var cameraMatrix = m4.yRotation(cameraAngleRadians);
     cameraMatrix = m4.translate(cameraMatrix, 0, 0, radius * 1.5);
+    var cameraPosition = [
+      cameraMatrix[12],
+      cameraMatrix[13],
+      cameraMatrix[14],
+    ]
+
+    var up = [0 ,1, 0];
+    cameraMatrix = m4.lookAt(cameraPosition, fPosition, up);
 
     var viewMatrix = m4.inverse(cameraMatrix);
     var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
@@ -549,7 +559,43 @@ var m3 = {
   },
 };
 
+function subtractVectors(a, b) {
+  return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+}
+
+function normalize(v) {
+  var length = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+  // make sure we don't divide by 0.
+  if (length > 0.00001) {
+    return [v[0] / length, v[1] / length, v[2] / length];
+  } else {
+    return [0, 0, 0];
+  }
+}
+
+function cross(a, b) {
+  return [a[1] * b[2] - a[2] * b[1],
+          a[2] * b[0] - a[0] * b[2],
+          a[0] * b[1] - a[1] * b[0]];
+}
+
 var m4 = {
+
+  lookAt: function(cameraPosition, target, up) {
+    var zAxis = normalize(subtractVectors(cameraPosition, target));
+    var xAxis = normalize(cross(up, zAxis));
+    var yAxis = normalize(cross(zAxis, xAxis));
+
+    return [
+      xAxis[0], xAxis[1], xAxis[2], 0,
+      yAxis[0], yAxis[1], yAxis[2], 0,
+      zAxis[0], zAxis[1], zAxis[2], 0,
+      cameraPosition[0],
+      cameraPosition[1],
+      cameraPosition[2],
+      1,
+   ]; 
+  },
 
   projection: function(width, height, depth) {
     // Note: This matrix flips the Y axis so 0 is at the top.
@@ -793,6 +839,16 @@ var m4 = {
             (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02))
     ];
   },
+
+  vectorMultiply: function(v, m) {
+    var dst = [];
+    for (var i = 0; i < 4; i++) {
+      dst[i] = 0.0;
+      for(var j = 0; j < 4; j++) {
+        dst[i] += v[j] * m[j * 4 + 1];
+      }
+    }
+  }
 
 };
 
